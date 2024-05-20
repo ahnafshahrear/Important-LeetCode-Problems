ecabc0f6b1b22f133df8ff456dbd089547e7f380
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-05-20 08:59:28 +0600

    remove-duplicates-from-sorted-array added

---
+++ Array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int> &nums) 
+    {
+        int index = 0;
+        int previous = INT_MIN;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != previous)
+            {
+                nums[index++] = nums[i];
+                previous = nums[i];
+            }
+        }
+        return index;
+    }
+};
