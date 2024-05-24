e95f4240dd3043e77ad76f5a9ddc35ed28aec520
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-05-24 19:45:48 +0600

    palindrome-linked-list added

---
+++ Linked List/palindrome-linked-list.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast and fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (fast and !fast->next)
+        {
+            slow = slow->next;
+        }
+
+        ListNode* previous = nullptr;
+        ListNode* next;
+
+        while (slow)
+        {
+            next = slow->next;
+            slow->next = previous;
+            previous = slow;
+            slow = next;
+        }
+        slow = previous;
+        fast = head;
+
+        while (fast and slow)
+        {
+            if (fast->val != slow->val)
+            {
+                return false;
+            }
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+};
+
+//... Time Complexity = O[n]
