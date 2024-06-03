b6128f7f066189f3dbd7e3a67540fa16ec3aac16
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-06-04 00:12:27 +0600

    power-of-three added

---
+++ Math/power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n)
+    {
+        while (n and n % 3 == 0)
+        {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+
+//... O[log(n)]
