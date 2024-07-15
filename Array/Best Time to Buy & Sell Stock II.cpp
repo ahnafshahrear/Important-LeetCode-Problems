ef6da8fb2b709e34348396183389b03d1914529f
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-07-15 10:14:18 +0600

    Best Time to Buy & Sell Stock II updated

---
+++ Array/Best Time to Buy & Sell Stock II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int buyingPrice = prices[0];
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < prices[i - 1]) {
+                profit += (prices[i - 1] - buyingPrice);
+                buyingPrice = prices[i];
+            }
+        }
+        profit += (prices.back() - buyingPrice);
+
+        return profit;
+    }
+};
