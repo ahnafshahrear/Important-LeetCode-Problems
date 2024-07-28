8740caccdaa8d769bbcacaeb99137c1bdf625f4d
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-07-28 22:26:36 +0600

    Pow(x, n) added

---
+++ Binary Search/Pow(x, n).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if (!n) {
+            return 1;
+        } else if (n < 0) {
+            return 1 / myPow(x, -n);
+        } else if (n & 1) {
+            return x * myPow(x * x, n / 2);
+        } else {
+            return myPow(x * x, n / 2);
+        }
+    }
+};
