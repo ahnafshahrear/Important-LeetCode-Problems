95d1223012bafeed2e07bda88db946fe759f469e
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-09-01 19:34:22 +0600

    Non-overlapping Intervals updated

--- Greedy/Non-overlapping Intervals.cpp
+++ Greedy/Non-overlapping Intervals.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>> &intervals) 
-    {
+    int eraseOverlapIntervals(vector<vector<int>> &intervals) {
         int size = intervals.size();
         int last = 0, count = 1;
 
         sort(intervals.begin(), intervals.end(),
-        [](vector<int> &x, vector<int> &y){
+        [](vector<int> &x, vector<int> &y) {
             return x[1] < y[1];
         });
-
-        for (int i = 1; i < size; i++)
-        {
-            if (intervals[i][0] >= intervals[last][1])
-            {
+        for (int i = 1; i < size; i++) {
+            if (intervals[i][0] >= intervals[last][1]) {
                 last = i; count++;
             }
         }
