8147b07baeb68791da62df02c531a265ec12513d
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-10-27 19:20:42 +0600

    Same Tree added

---
+++ Trees/Same Tree.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p and !q) return true;
+        if (!p or !q or p->val != q->val) return false;
+        return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+    }
+};
