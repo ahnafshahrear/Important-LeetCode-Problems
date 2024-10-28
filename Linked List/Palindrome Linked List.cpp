eb584d7e1f6a712ed67ef2d479549a6b265d77fe
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-10-28 10:37:55 +0600

    Palindrome Linked List updated

--- Linked List/Palindrome Linked List.cpp
+++ Linked List/Palindrome Linked List.cpp
@@ -1,43 +1,15 @@
 class Solution {
+private:
+    ListNode* node;
+    bool checkPalindrome(ListNode* head) {
+        if (head == nullptr) return true;
+        bool flag = checkPalindrome(head->next) and (head->val == node->val);
+        node = node->next;
+        return flag;
+    }
 public:
-    bool isPalindrome(ListNode *head)
-    {
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while (fast and fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        if (fast and !fast->next)
-        {
-            slow = slow->next;
-        }
-
-        ListNode* previous = nullptr;
-        ListNode* next;
-
-        while (slow)
-        {
-            next = slow->next;
-            slow->next = previous;
-            previous = slow;
-            slow = next;
-        }
-        slow = previous;
-        fast = head;
-
-        while (fast and slow)
-        {
-            if (fast->val != slow->val)
-            {
-                return false;
-            }
-            fast = fast->next;
-            slow = slow->next;
-        }
-        return true;
+    bool isPalindrome(ListNode* head) {
+        node = head;
+        return checkPalindrome(head);
     }
 };
