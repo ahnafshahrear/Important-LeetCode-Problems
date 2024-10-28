c538b76b1bbf4f18fd51e2472c84343fd67a1606
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-10-28 10:12:13 +0600

    Validate Binary Search Tree added

---
+++ Binary Search Tree/Validate Binary Search Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+    bool isValid(TreeNode* root, long long int small, long long int big) {
+        if (root == nullptr) return true;
+        if (root->val >= big or root->val <= small) return false;
+        return isValid(root->left, small, root->val) and isValid(root->right, root->val, big);
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, INT64_MIN, INT64_MAX);    
+    }
+};
