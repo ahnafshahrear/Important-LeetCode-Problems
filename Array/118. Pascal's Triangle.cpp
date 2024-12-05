8ac0255ed1a76db8cbe0d2bc47484afe3b19ce9d
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-05 18:59:49 +0600

    Create 118. Pascal's Triangle.cpp

---
+++ Array/118. Pascal's Triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        result.push_back({1});
+
+        for (int i = 1; i < numRows; i++) {
+            result.push_back({1});
+            for (int k = 1; k < result[i - 1].size(); k++) {
+                result[i].push_back(result[i - 1][k - 1] + result[i - 1][k]);
+            }
+            result[i].push_back(1);
+        }
+        return result;
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/results-triangle/description/
+// Time Complexity: O(n)
