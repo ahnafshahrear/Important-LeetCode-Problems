49abd0cc187bf02bd41ecef59c3e8df6a5bb2784
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-06 17:09:21 +0600

    Create 142. Linked List Cycle II.cpp

---
+++ Linked List/142. Linked List Cycle II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while (fast and fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                while (head != slow) {
+                    head = head->next;
+                    slow = slow->next;
+                }
+                return head;
+            }
+        }
+        return nullptr;
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/linked-list-cycle/description/
+// Time Complexity: O(n)
