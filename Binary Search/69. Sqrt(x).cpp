ad983e7e56d647ff2e3d2babd82c63a0387f8df9
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-06 20:11:51 +0600

    Create 69. Sqrt(x).cpp

---
+++ Binary Search/69. Sqrt(x).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int number) {
+        double eps = 1e-7;
+        double low = 0, high = number;
+
+        while (high - low > eps) {
+            double mid = (low + high) / 2;
+            if (mid * mid < number) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/sqrtx/description/
+// Time Complexity: O(logn)
