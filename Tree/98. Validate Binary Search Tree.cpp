b4729cc6cb8b8862b4322a9fa8f23bc0fed12372
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-06 09:53:41 +0600

    Create 98. Validate Binary Search Tree.cpp

---
+++ Tree/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    bool isValid(TreeNode* root, long long int small, long long int big) {
+        if (root == nullptr) return true;
+        if (root->val >= big or root->val <= small) return false;
+        return isValid(root->left, small, root->val) and isValid(root->right, root->val, big);
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, INT64_MIN, INT64_MAX);    
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/validate-binary-search-tree/description/
+// Time Complexity: O(n)
