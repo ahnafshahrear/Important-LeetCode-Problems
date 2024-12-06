9e44c95c04da83fe670e0095724202ff3ae02d18
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-06 10:03:00 +0600

    Create 110. Balanced Binary Tree.cpp

---
+++ Tree/110. Balanced Binary Tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        checkBalanced(root);
+        return balanced;
+    }
+private:
+    bool balanced = true;
+
+    int checkBalanced(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = checkBalanced(root->left), right = checkBalanced(root->right);
+
+        if (abs(left - right) > 1) balanced = false;
+
+        return 1 + max(left, right);
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/balanced-binary-tree/description/
+// Time Complexity: O(n)
