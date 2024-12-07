6f0e7b89f14b59a36d577c02fd35289722a723ca
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-07 06:05:23 +0600

    Update and rename 88. Merge Sorted Array.cpp to Merge Sorted Array.cpp

--- Array/Merge Sorted Array.cpp
+++ Array/Merge Sorted Array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j = m - 1;
-        int k = n - 1;
+        int j = m - 1, k = n - 1;
 
         for (int i = m + n - 1; k >= 0; i--) {
             (j >= 0 and nums1[j] > nums2[k]) ? nums1[i] = nums1[j--] : nums1[i] = nums2[k--];
