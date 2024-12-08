3abebd14b216db92012190b56f2acfaabd5da672
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-12-08 19:54:24 +0600

    Create Valid Palindrome II.cpp

---
+++ String/Valid Palindrome II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindromeRange(const string &s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return isPalindromeRange(s, i + 1, j) or isPalindromeRange(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/valid-palindrome-ii/description/
+// Time Complexity: O(n)
